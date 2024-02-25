ba1162c7511cee1a44dadd944ab56f0bd41d1cc6
Dhruv Khullar <dhruv.khullar@softwarerisk.com>
2024-02-25 13:16:00 +1100

    1. added a method to add jobs 2. Added struct and methods for timer for frequency control

--- batcher.go
+++ batcher.go
@@ -1,6 +1,7 @@
 package gomicrobatcher
 
 import (
+	"errors"
 	"sync"
 	"time"
 )
@@ -31,8 +32,8 @@ type GenericBatch struct {
 type genericBatcher struct {
 	processor JobProcessor
 	batchSize int
-	linger    time.Duration
-	pending   *GenericBatch
+	frequency    time.Duration
+	unresolved   *GenericBatch
 	mu        sync.Mutex
 	shutdown  bool
 }
@@ -43,8 +44,91 @@ type jobValue struct {
 	resultCh chan interface{}
 }
 
-// timerWithStop is a timer with additional control.
+// timerWithStop is a timer with and an additional channel to indicate that it needs to stop
 type timerWithStop struct {
 	timer  *time.Timer
 	stopCh chan bool
+}
+// The job gets returned with an indicating channel 
+type genericJobResult struct {
+	ch    chan interface{}
+	value interface{}
+}
+
+var ErrorMessage = errors.New("shutting down batcher")
+
+func NewBatcher(customProcessor JobProcessor, size int, time time.Duration) Batcher {
+
+	return &genericBatcher{
+		processor: customProcessor, 
+		batchSize: size, 
+		frequency: time, 
+		mu: sync.Mutex{},
+		shutdown: false,
+		unresolved: nil,	
+	}
+}
+
+/*
+In summary, this function adds a job to the batching system. 
+If a batch is not already unresolved, it creates a new batch with the current job. 
+If a batch is unresolved, it adds the job to the existing batch. 
+If the batch size is reached, it triggers the execution of the batch. 
+The function returns a JobResult instance, which can be used to retrieve the result of the job.
+*/
+func (b *genericBatcher) AddJob(job interface{}) (JobResult, error) {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+
+	if b.shutdown {
+		return nil, ErrorMessage
+	}
+
+	resultCh := make(chan interface{}, 1)
+	jobVal := jobValue{value: job, resultCh: resultCh}
+
+	if b.unresolved == nil {
+		// b.unresolved = b.newBatch(jobVal)
+		return &genericJobResult{ch: resultCh}, nil
+	}
+
+	b.unresolved.values = append(b.unresolved.values, jobVal)
+
+	if len(b.unresolved.values) >= b.batchSize {
+		b.unresolved.stop()
+		b.unresolved = nil
+	}
+
+	return &genericJobResult{ch: resultCh}, nil
+}
+
+
+// this time stops the batch of jobs
+func (b *GenericBatch) stop() {
+	b.timer.stop()
+}
+
+// stop the actual timer 
+func (t *timerWithStop) stop() {
+	t.stopCh <- true
+	t.timer.Stop()
+}
+
+//helps to get the result of a single job when it is completed
+func (j *genericJobResult) GetValue() interface{} {
+	if j.value == nil {
+		j.value = <-j.ch
+		close(j.ch)
+	}
+	return j.value
+}
+
+func (b *genericBatcher) Stop() {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+	b.shutdown = true
+	if b.unresolved != nil {
+		b.unresolved.stop()
+		b.unresolved = nil
+	}
 }
